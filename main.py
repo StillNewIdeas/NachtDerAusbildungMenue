ab9b6c8b5d994f45c30318e441e3aad2cdbd807a
StillNewIdeas <russellrabtattan@gmail.com>
2024-05-24 11:50:33 +0200

    Implemented Caesar-Encryption

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-
+from os import system
 
 def caesar_explanation():
-    print("Wow, hier eine voll krasse Erklärung über die Caesar verschlüsselungsmethode")
+    print("Die Caesar-Verschlüsselung ist ein einfaches Verschlüsselungsverfahren, \nbei dem jeder Buchstabe im Klartext um eine feste Anzahl \nvon Stellen im Alphabet verschoben wird. \nZum Beispiel, bei einer Verschiebung um 3: \nA -> D, \nB -> E,\nC -> F.")
 
+    caesar_encryption()
+
+def caesar_encryption():
 
 
     to_encrypt = input("Gib eine Nachricht ein: \n")
+    #Faktor selbst eingeben
+    print("Für diese Verschlüsselung wird jetzt mit dem Faktor 10 verschlüsselt.")
+    n=10
+    ans = ""
+    for i in range(len(to_encrypt)):
+        ch = to_encrypt[i]
+        
+        # check if space is there then simply add space
+        if ch==" ":
+            ans+=" "
+        # check if a character is uppercase then encrypt it accordingly 
+        elif (ch.isupper()):
+            print(ch, "wird zu", chr((ord(ch) + n-65) % 26 + 65))
+            ans += chr((ord(ch) + n-65) % 26 + 65)
+        # check if a character is lowercase then encrypt it accordingly
+        
+        else:
+            print(ch, "wird zu", chr((ord(ch) + n-97) % 26 + 97))
+            ans += chr((ord(ch) + n-97) % 26 + 97)
+    
+        
     #encryption stuff
-    print("Und das ist das Ergebnis: ",to_encrypt)
+    print("Und das ist das Ergebnis: ",ans)
 
-    print("")
+    print("\n\n")
 
     main()
 
